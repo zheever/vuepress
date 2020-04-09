cd24bd2db98bb2e6b9f9eb0ae8ca40c71824c8dd
HUANGZHE <huangzhe@kingsoft.com>
2020-04-09 15:39:58 +0800

    add sidebar generate script netfily collections setting

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,17 @@
+const path = require("path");
+const rootpath = path.dirname(__dirname); //执行一次dirname将目录定位到docs目录
+const utils = require('../../scripts/utils.js');
+const filehelper = require('../../scripts/initPage.js');
+
 module.exports = {
   title: 'Netlify CMS + VuePress',
   description: 'Netlify + VuePress',
   themeConfig: {
     docsDir: 'docs',
     repo: 'zheever/vuepress',
-    sidebar: [
-      '/',
-      '/hhhh'
-    ],
+    sidebar: {
+      '/doc/css/': utils.genSidebar('css', filehelper.getFileName(rootpath+"/css"), false)
+    },
     nav: [
       {
         text: 'Admin',
