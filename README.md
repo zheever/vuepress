50070753e0e4716e8aac9faf87ba9d97e327bbff
HUANGZHE <huangzhe@kingsoft.com>
2020-04-09 09:40:19 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# vuepress
+# vuepress-netlify-cms
+Example integration of VuePress with Netlify
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'Netlify CMS + VuePress',
+  description: 'Netlify + VuePress',
+  themeConfig: {
+    docsDir: 'docs',
+    repo: 'zheever/vuepress',
+    sidebar: [
+      '/',
+      '/hhhh'
+    ],
+    nav: [
+      {
+        text: 'Admin',
+        link: '/admin/#/',
+      }
+    ]
+  }
+}
